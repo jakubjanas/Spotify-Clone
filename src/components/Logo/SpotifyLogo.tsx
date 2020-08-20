31553c367a914395a730aecfe07b650a3750c6f1
Jakub Janas <jakub.janas1@gmail.com>
2020-08-20 21:26:44 +0200

    Added spotify logo coponent

---
+++ src/components/Logo/SpotifyLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SpotifyLogo.css';
+
+const SpotifyLogo = () => {
+    return (
+        <div>
+            <img src="/Spotify_Logo.png" className={"logo"}/>
+        </div>);
+}
+
+export default SpotifyLogo;
