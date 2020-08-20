68581cdcc19d6b36d7cc1c23f53830bd2f39e727
Jakub Janas <jakub.janas1@gmail.com>
2020-08-20 21:26:59 +0200

    Added sidebar component

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SpotifyLogo from '../Logo/SpotifyLogo';
+
+const SideBar = () => {
+    return (
+        <div>
+            <SpotifyLogo />
+        </div>
+    );
+}
+
+export default SideBar;
