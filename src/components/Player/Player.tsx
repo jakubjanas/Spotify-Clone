cf4554ea24ac0e1a5650f155b96b01d7e452675e
Jakub Janas <jakub.janas1@gmail.com>
2020-09-17 22:06:55 +0200

    Added player icons

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Player.css';
+import { ReactComponent as RepeatIcon } from '../../assets/icons/repeatIcon.svg';
+import { ReactComponent as NextIcon } from '../../assets/icons/nextIcon.svg';
+import { ReactComponent as PlayIcon } from '../../assets/icons/playIcon.svg';
+import { ReactComponent as ShuffleIcon } from '../../assets/icons/shuffleIcon.svg';
 
 const Player = () => {
     return (
         <div className={'player'}>
-         Player
+            <ShuffleIcon className={'shuffleIcon'}/>
+            <NextIcon className={'previousIcon'}/>
+            <PlayIcon className={'playIcon'}/>
+            <NextIcon className={'nextIcon'}/>
+            <RepeatIcon className={'repeatIcon'}/>
         </div>
     );
 }
