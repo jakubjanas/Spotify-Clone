c1fd409609141dac823373047d29349170ba3c38
Jakub Janas <jakub.janas1@gmail.com>
2020-10-10 19:29:20 +0200

    Repeat button added

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Player.css';
-import { ReactComponent as RepeatIcon } from '../../assets/icons/repeatIcon.svg';
+
 import { ReactComponent as NextIcon } from '../../assets/icons/nextIcon.svg';
 import { ReactComponent as PlayIcon } from '../../assets/icons/playIcon.svg';
 import ProgressBar from './ProgressBar/ProgressBar';
 import ShuffleButton from './Buttons/ShuffleButton';
+import RepeatButton from './Buttons/RepeatButton';
 
 const Player = () => {
     return (
@@ -13,7 +14,7 @@ const Player = () => {
             <NextIcon className={'previousIcon'}/>
             <PlayIcon className={'playIcon'}/>
             <NextIcon className={'nextIcon'}/>
-            <RepeatIcon className={'repeatIcon'}/>
+            <RepeatButton />
             {/* <ProgressBar /> */}
         </div>
     );
