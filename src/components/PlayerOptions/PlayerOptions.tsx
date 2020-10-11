8c19eb80caa314a59861b3479874a918010620c0
Jakub Janas <jakub.janas1@gmail.com>
2020-10-11 20:29:19 +0200

    PlayerOpetions icons added

--- src/components/PlayerOptions/PlayerOptions.tsx
+++ src/components/PlayerOptions/PlayerOptions.tsx
@@ -1,10 +1,16 @@
+import { ReactComponent } from '*.svg';
 import React from 'react';
 import './PlayerOptions.css';
+import { ReactComponent as PlaylistIcon } from '../../assets/icons/playlistIcon.svg';
+import { ReactComponent as WorkstationIcon } from '../../assets/icons/workstationIcon.svg';
+import { ReactComponent as FullVolumeIcon } from '../../assets/icons/fullVolumeIcon.svg';
 
 const PlayerOptions = () => {
     return (
         <div className={'playerOptions'}>
-         Player options
+            <PlaylistIcon />
+            <WorkstationIcon />
+            <FullVolumeIcon />
         </div>
     );
 }
