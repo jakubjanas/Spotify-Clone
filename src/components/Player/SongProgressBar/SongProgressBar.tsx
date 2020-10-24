b908787a19a367486f2a4405eca82d48e6cfe941
Jakub Janas <jakub.janas1@gmail.com>
2020-10-24 21:56:39 +0200

    css fix

--- src/components/Player/SongProgressBar/SongProgressBar.tsx
+++ src/components/Player/SongProgressBar/SongProgressBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SongProgressBar.css';
-import { Row, Col } from 'reactstrap';
+import { Col } from 'reactstrap';
 import ProgressBar from '../../ProgressBar/ProgressBar';
 
 const SongProgressBar = () => {
