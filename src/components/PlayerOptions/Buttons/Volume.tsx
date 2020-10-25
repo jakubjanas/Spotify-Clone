4e2562342b9877249ed9d398a5caa6e0f0c5cdcb
Jakub Janas <jakub.janas1@gmail.com>
2020-10-25 21:36:07 +0100

    added volume state

--- src/components/PlayerOptions/Buttons/Volume.tsx
+++ src/components/PlayerOptions/Buttons/Volume.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProgressBar from '../../ProgressBar/ProgressBar';
 import { ReactComponent as FullVolumeIcon } from '../../../assets/icons/fullVolumeIcon.svg';
 import './Buttons.css';
 
 const Volume = () => {
-    const completed = 20;
+    const [status, setStatus] = useState(true);
+    const [value, setValue] = useState(50);
+
+    useEffect(() => {
+        if(status) {
+            setVolumeValue(50);
+        }
+        else
+        {
+            setVolumeValue(0);
+        }
+        
+    }, [status]);
+
+    const setVolumeValue = (volumeValue: number) => {
+        const value = Math.min(100, Math.max(0, volumeValue));
+        setValue(value);
+    }
 
     return (
         <div className={'volume'}>
-            <FullVolumeIcon className={'icon'}/>
+            <FullVolumeIcon className={'icon'} onClick={() => setStatus(prev => !prev)}/>
             <div className={'regulator'}>
-                <ProgressBar completed={completed} />
+                <ProgressBar completed={value} />
             </div>
         </div>
     )
