c11f67f7e17f08e1641b7c0361a65f0a25d7880e
Jakub Janas <jakub.janas1@gmail.com>
2020-10-28 19:47:59 +0100

    Configure prettier formatter

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -2,32 +2,31 @@ import React, { useRef, useState } from 'react';
 import './ProgressBar.css';
 
 type Props = {
-    completed: number;
-    setCompleted: (value: number) => void;
-}
+	completed: number;
+	setCompleted: (value: number) => void;
+};
 
-const ProgressBar = ({completed, setCompleted} : Props) => {
-    const refP = useRef<HTMLDivElement>(null);
+const ProgressBar = ({ completed, setCompleted }: Props) => {
+	const refP = useRef<HTMLDivElement>(null);
 
-    const handleOnClick = (e: React.MouseEvent) => {
-        if(refP.current)
-        {
-            const rect = refP.current.getBoundingClientRect();
-            const offset = Math.floor(rect.left);
-            const value = Math.floor(((e.pageX - offset) / rect.width) * 100)
+	const handleOnClick = (e: React.MouseEvent) => {
+		if (refP.current) {
+			const rect = refP.current.getBoundingClientRect();
+			const offset = Math.floor(rect.left);
+			const value = Math.floor(((e.pageX - offset) / rect.width) * 100);
 
-            setCompleted(value);
-        }
-    }
+			setCompleted(value);
+		}
+	};
 
-    return (
-        <div className={'progressBar'} onClick={handleOnClick} ref={refP}>
-            <div className={'progressFiller'} style={{width:`${completed}%`}} />
-            <svg height="10" width="10">
-                <circle cx="5" cy="5" r="5" stroke="empty" fill="white" />
-            </svg>
-        </div>
-    );
-}
+	return (
+		<div className={'progressBar'} onClick={handleOnClick} ref={refP}>
+			<div className={'progressFiller'} style={{ width: `${completed}%` }} />
+			<svg height='10' width='10'>
+				<circle cx='5' cy='5' r='5' stroke='empty' fill='white' />
+			</svg>
+		</div>
+	);
+};
 
 export default ProgressBar;
