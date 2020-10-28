27a4afc904cdbc26840a0fdfdcc6af85c8b45038
Jakub Janas <jakub.janas1@gmail.com>
2020-10-28 20:29:13 +0100

    Current played song base layout created

--- src/components/CurrentSong/CurrentSong.tsx
+++ src/components/CurrentSong/CurrentSong.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { ReactComponent as CdIcon } from '../../assets/icons/cdIcon.svg';
+import './CurrentSong.css';
 
 export const CurrentSong = () => {
-	return <div>CurrentSong</div>;
+	return (
+		<div className={'currentSong'}>
+			<div className={'songCover'}>
+				<CdIcon />
+			</div>
+			<div>Song name</div>
+			<div>icons</div>
+		</div>
+	);
 };
