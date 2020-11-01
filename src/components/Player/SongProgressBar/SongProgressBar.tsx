8cdadc30dcdefebd1ba2dc6fc631d0461801e896
Jakub Janas <jakub.janas1@gmail.com>
2020-11-01 20:29:03 +0100

    Added time formatter

--- src/components/Player/SongProgressBar/SongProgressBar.tsx
+++ src/components/Player/SongProgressBar/SongProgressBar.tsx
@@ -4,19 +4,43 @@ import { Col } from 'reactstrap';
 import { ProgressBar } from '../../ProgressBar';
 
 export const SongProgressBar = () => {
+	const songLenght = 195;
+	const [songTime, setSongTime] = useState(songLenght);
 	const [completed, setCompleted] = useState(100);
+
+	const onClickHandler = (completedValue: number) => {
+		setCompleted(completedValue);
+		setSongTime(calculateCurentSongTime(completedValue, songLenght));
+	};
 
 	return (
 		<div className={'songBar'}>
 			<div className={'time'}>
-				<span>2:27</span>
+				<span>{toTimeFormat(songTime)}</span>
 			</div>
 			<Col>
-				<ProgressBar completed={completed} setCompleted={setCompleted} />
+				<ProgressBar completed={completed} setCompleted={onClickHandler} />
 			</Col>
 			<div className={'time'}>
-				<span>3:15</span>
+				<span>{toTimeFormat(songLenght)}</span>
 			</div>
 		</div>
 	);
+};
+
+const calculateCurentSongTime = (
+	completedValue: number,
+	songLenght: number
+) => {
+	return Math.round((songLenght * completedValue) / 100);
+};
+
+const toTimeFormat = (time: number) => {
+	const date = new Date(new Date().setMinutes(0, time));
+
+	return `${date.getMinutes()}:${toSecondsFormat(date.getSeconds())}`;
+};
+
+const toSecondsFormat = (seconds: number) => {
+	return seconds < 10 ? '0' + seconds : seconds;
 };
