0d7e9e3f951fc56fa01b3f7be364aae5efa2bd86
Jakub Janas <jakub.janas1@gmail.com>
2020-11-04 19:43:18 +0100

    added login page

---
+++ src/pages/Login/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const LoginPage = () => {
+	return (
+		<div>
+			<h1>Unauthorize</h1>
+		</div>
+	);
+};
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { HomePage } from './Home/HomePage';
 export { LibraryPage } from './Library/LibraryPage';
 export { SearchPage } from './Search/SearchPage';
+export { LoginPage } from './Login/LoginPage';
